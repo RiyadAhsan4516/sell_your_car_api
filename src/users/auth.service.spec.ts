f0734b3dd737e5d555e25a2d08602e58a32ed2fe
riyad <riyad.ahsan4516@gmail.com>
2024-04-19 22:27:17 +0600

    - wrote test cases for auth.service

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,18 +1,43 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test, TestingModule, TestingModuleBuilder } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { UsersService } from "./users.service";
+import { User } from "./user.entity";
 
-describe('AuthService', () => {
+describe("AuthService", () => {
   let service: AuthService;
 
+  // BEFORE EACH SETS UP ALL THE DECLARATIONS AND DEPENDENCY INJECTION, BEFORE WE START TESTING
   beforeEach(async () => {
+    // Create a fake copy of the module, auth.service depends on
+    const fakeUsersService: Partial<UsersService> = {
+      getOne: (id: number) => Promise.resolve({} as User),
+      create: (email: never, password: never) => Promise.resolve({ message: "user created" }),
+      getMany: (email: string)=> Promise.resolve([] as User[])
+    };
+
+    // Create a module of the auth.service
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService
+        }
+      ]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+
+  // THESE ARE THE ACTUAL TESTS. WE TEST USING THE IT FUNCTIONS
+  it("can create an instance of auth service", async () => {
     expect(service).toBeDefined();
   });
+
+  // NOW CREATE YOUR OWN TESTS
+  it("Creates a new user and returns a message", async()=>{
+    await service.signUp("riyad@gmail.com", "helloWorld")
+  })
+
 });
